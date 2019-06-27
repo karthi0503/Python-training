8b592c8cd9384425252a1303cb9babc87c1b906f
karthi0503 <52058089+karthi0503@users.noreply.github.com>
2019-06-27 08:23:15 +0530

    Add files via upload

---
+++ eigth day training.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 27 08:18:52 2019
+
+@author: WELCOME
+"""
+
+#dictionaries
+
+dictions={'name':'Karthikeyan','id':613}
+
+print(dictions['name'])
+print(dictions['id'])
